46fe8f4691f3d01d7efc3a5d44f00fb8387e21fd
bytrix <wengzhijie@126.com>
2021-12-19 16:10:27 +0800

    delete useless code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>You have selected {{selectedData.length}} items.</h1>
+    <el-table @select="onSelect" ref="tableRef" :data="tableData" style="width: 100%">
+      <el-table-column type="selection"></el-table-column>
+      <el-table-column prop="id" label="ID" width="60px" />
+      <el-table-column prop="login" label="Username" width="120px" />
+      <el-table-column prop="url" label="Github" />
+    </el-table>
+    <el-pagination
+      :total="rows"
+      @current-change="onPageChange"
+    ></el-pagination>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import { nextTick } from '@vue/runtime-core'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      rows: 0,
+      selectedData: [],
+      tableData: [],
+    }
+  },
+  mounted() {
+    this.getData(1)
+  },
+  methods: {
+    getData(page) {
+      return axios.get('https://www.fastmock.site/mock/25671bee3420beb504088201c7f40a38/test/api/user?page=' + page)
+        .then(({ data }) => {
+          this.tableData = data.data
+          this.rows = data.rows
+          nextTick(() => {
+            this.tableData.forEach(row => {
+              if(this.selectedData.find(item => item.id === row.id)) {
+                this.$refs.tableRef.toggleRowSelection(row)
+              }
+            })
+          })
+        })
+    },
+    onPageChange(page) {
+      this.getData(page)
+    },
+    onSelect(selection) {
+      this.selectedData = selection
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
